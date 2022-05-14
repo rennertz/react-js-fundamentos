2684d9a3be55d42654db805078186436f3951522
renner <rennertramos@gmail.com>
2022-05-14 17:36:27 -0300

    children para Botao e props para Item

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component {
+class Botao extends React.Component{
   render() {
     return (
-      <button className={ style.Botao }>
-        Botão
+      <button className={ style.botao }>
+        {this.props.children}
       </button>
     )
   }
